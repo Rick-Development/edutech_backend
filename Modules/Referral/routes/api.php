b0eafb8bb6ee425afff24b97a9fcfd4cd1ced38d
rickdevelopmentcompany <rickdevelopmentcompany>
2025-11-01 01:04:52 +0100

    changes

--- Modules/Referral/routes/api.php
+++ Modules/Referral/routes/api.php
@@ -7,4 +7,5 @@ use Modules\Referral\Http\Controllers\ReferralController;
 Route::middleware(['auth:sanctum'])->prefix('referral')->group(function () {
     Route::get('/dashboard', [ReferralController::class, 'getDashboard']);
     Route::get('/history', [ReferralController::class, 'getHistory']);
+    Route::get('/history3', [ReferralController::class, 'getHistory']);
 });
